53bdb063014e65d3b6a71a929df48e5184ec5142
Manali Acharya <manali@impacttechlab.com>
2026-06-16 11:11:14 +0530

    book sync api calling

--- app/Modules/BookSync/app/Providers/RouteServiceProvider.php
+++ app/Modules/BookSync/app/Providers/RouteServiceProvider.php
@@ -27,6 +27,6 @@ class RouteServiceProvider extends ServiceProvider
 
     protected function mapApiRoutes(): void
     {
-        Route::middleware('api')->prefix('api')->name('api.')->group(module_path($this->name, '/routes/api.php'));
+        Route::group([], module_path($this->name, '/routes/api.php'));
     }
 }
